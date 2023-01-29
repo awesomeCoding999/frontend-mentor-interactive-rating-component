8141c7d21851657ae444675be2b6399e5bab2d23
CodingChick <awesomecoding26@gmail.com>
2023-01-29 00:16:28 -0800

    chore: setting up basic file strucuture

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+export const Footer = () => {
+  return (
+    <footer>
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://www.frontendmentor.io/profile/awesomeCoding999">
+        CodingChick
+      </a>
+      .
+    </footer>
+  );
+};
